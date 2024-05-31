2ff4297b066ba5acaee192ac7501a31c102a1840
umeshturnkey <umesh.kumar@turnkeytix.com>
2024-05-31 17:08:59 +0530

    fix addMonthlyData route

---
+++ pages/addMonthlyData/index.js
@@ -0,0 +1,22 @@
+import Layout from "@/components/Layout"
+import Header from "@/components/Layout/Header"
+import DataEntryContainer from "@/page-containers/DateEntry/addprojectdataentry"
+
+const DataEntryPage=({childern})=> {
+  return (
+    <>
+   <Header/>
+   <Layout><DataEntryContainer/></Layout>
+    </>
+  )
+}
+export async function getServerSideProps(context) {
+
+  return {
+    props: {
+     
+    },
+  }
+}
+
+export default DataEntryPage
