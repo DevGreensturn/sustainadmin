9300ef8a0a0a07c96a8c4ccf13f64d3c33a15f20
Purchase Metaverk <satyendra.kumar@metaverk.io>
2024-06-04 11:42:15 +0530

    UI Update

--- page-containers/DateEntry/EnergyComsuption/reductionEnergyTable.js
+++ page-containers/DateEntry/EnergyComsuption/reductionEnergyTable.js
@@ -7,8 +7,7 @@ import { FaRegEdit } from "react-icons/fa";
 import { RiFilter2Fill } from "react-icons/ri";
 import { Modal, Button } from 'react-bootstrap';
 
-const Utilityprovidertable =()=>{
-
+const ReductionEnerguTable =()=>{
     const [show, setShow] = useState(false);
     const handleClose =()=> setShow(false);
     const handleShow =()=>setShow(true);
@@ -21,56 +20,34 @@ const Utilityprovidertable =()=>{
     const handleCloseDelete =()=> setShowDelete(false);
     const handleShowDelete =()=> setShowDelete(true);
 
+
     const columns = [
     	
         {
-            name: <b>Energy Type</b>,
-            selector: (row) => row.energyType,
+            name: <b>Initiative Name</b>,
+            selector: (row) => row.initiativename,
             wrap:"true"
         },
         {
-            name: <b className="text-center">Meter No. </b>,
-            selector: (row) => row.meterNumber,
+            name: <b className="text-center">Source </b>,
+            selector: (row) => row.source,
             wrap:"true"
         },
         {
-            name: <b className="text-center">Account No.</b>,
-            selector: (row) => row.accountNumber,
+            name: <b className="text-center">Energy Amount</b>,
+            selector: (row) => row.energyamount,
             wrap:"true"
         },
         {
-            name: <b>Service Provider</b>,
-            selector: (row) => row.serviceProvider,
-            wrap:"true"
-        },
-        
-        {
-            name: <b>Reading Date</b>,
-            selector: (row) => row.readingDate,
-            wrap:"true"
+            name: <b>Unite</b>,
+            selector: (row) => row.unite,
+            wrap:"true",
+            
         },
 
         {
-            name: <b>Meter Reading</b>,
-            selector: (row) => row.meterReading,
-            wrap:"true"
-        },
-
-        {
-            name: <b>Consumption</b>,
-            selector: (row) => row.consumption,
-            wrap:"true"
-        },
-
-        {
-            name: <b>Unit</b>,
-            selector: (row) => row.unit,
-            wrap:"true"
-        },
-
-        {
-            name: <b>Supporting Document</b>,
-            selector: (row) => row.supportingDocument,
+            name: <b>Calculation Method</b>,
+            selector: (row) => row.calculationmethod,
             wrap:"true"
         },
 
@@ -84,42 +61,32 @@ const Utilityprovidertable =()=>{
     
     const rows = [
         {
-            
-            energyType: "Electricity",
-            meterNumber: "00001",
-            accountNumber: "1234567890",
-            serviceProvider: "DEWA",
-            readingDate: "10 March 2024",
-            meterReading: "1344",
-            consumption: "1344",
-            unit:"Joule",
-            supportingDocument:"2 attachments",
-            reportStatus :<div><button type="btn" className="btn btn-sm btn-outline-secondary">Submitted</button></div>,
+            initiativename: "LED Lighting",
+            source: "Energy Consumption-Utility Provider Energy",
+            energyamount: "1234567890",
+            unite:"Joule",
+            calculationmethod:"Estimated",
             Action :<div className="d-flex align-items-center"><FaRegEdit style={{color:"secondary", fontSize:"20px"}} onClick={handleShowEdit}/>  <MdDeleteForever icon={faTimes} className="mx-2" style={{color:"red", fontSize:"20px"}} onClick={handleShowDelete}/> </div>
         },
+
          {
-            
-            energyType: "Cooling",
-            meterNumber: "00002",
-            accountNumber: "1234567890",
-            serviceProvider: "DEWA",
-            readingDate: "23rd March 2024",
-            meterReading: "1452",
-            consumption: "1452",
-            unit:"kWh",
-            supportingDocument:"2 attachments",
-            reportStatus :<div><button type="btn" className="btn btn-sm btn-outline-success">Audited</button></div>,
+            initiativename: "Fuel Optimization",
+            source: "Energy Consumption-Non Renewable Resource",
+            energyamount: "1234567890",
+            unite:"kWh",
+            calculationmethod:"Modeled",
+
             Action :<div className="d-flex align-items-center"><FaRegEdit style={{color:"secondary", fontSize:"20px"}}/>  <MdDeleteForever icon={faTimes} className="mx-2" style={{color:"red", fontSize:"20px"}}/> </div>    
          },
     ];
 
-
-    return(
+    return (
+       
         <section>
                 <div className="d-flex align-items-center justify-content-between">
                     <div className="d-flex align-items-center">
-                    <img src="../Images/utilityProvider.png" alt="" />
-                    <h5 className="mx-2">Utility Provider Energy</h5>
+                    <img src="../Images/ReductionEnergyTable.png" alt="" />
+                    <h5 className="mx-2">Reduction of Energy Consumption</h5>
                     </div>
                     <div>
                         <button type="btn" className="btn btn-outline-success" onClick={handleShow}>
@@ -153,85 +120,68 @@ const Utilityprovidertable =()=>{
                         <>
                        <div>
                         <div className="d-flex align-items-center">
-                            <img src="../Images/EnergyComsuption_Modal.png" alt="" className="img-fluid"/>
+                            <img src="../Images/soldEnergy_Modal.png" alt="" className="img-fluid"/>
                             <div className="mx-2">
                                 <h4>Energy Consumption</h4>
-                                <h5>Utility Provider Energy</h5>
+                                <h5>Reduction of Energy Consumption</h5>
                             </div>
-
                         </div>
 
                         <div className="row mt-3">
                             <div className="col-md-4">
-                                <label htmlFor="">Energy Type</label>
-                                <select className="form-select" aria-label="Default select example">
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                <label htmlFor="">Initiative Name</label>
+                                <input type="date" className="form-control" placeholder="LED Lighting" />
+                            </div>
+
+                            <div className="col-md-4">
+                            <label htmlFor="">Source</label>
+                            <select className="form-select" aria-label="Default select example">
+                                    <option selected>Energy Consumption - Utility Provider Energy</option>
+                                    <option value="1">Wind</option>
                                 </select>
                             </div>
 
                             <div className="col-md-4">
-                                <label htmlFor="">Meter No.</label>
-                                <input type="text" className="form-control" placeholder="12345" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Account No.</label>
-                                <input type="text" className="form-control" placeholder="1234567890" />
+                            <label htmlFor="">Energy Amount</label>
+                                <input type="text" className="form-control" placeholder="24271"/>
                             </div>
 
                         </div>
 
                         <div className="row mt-3">
                             <div className="col-md-4">
-                                <label htmlFor="">Service Provider</label>
-                                <input type="text" className="form-control" placeholder="DAWA" />
+                                <label htmlFor="">Sold Energy</label>
+                                <input type="text" className="form-control" placeholder="264271" />
                             </div>
 
                             <div className="col-md-4">
-                                <label htmlFor="">Reading Date</label>
-                                <input type="text" className="form-control" placeholder="10 March 2024" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Meter Reading</label>
-                                <input type="text" className="form-control" placeholder="1234" />
-                            </div>
-
-                        </div>
-
-                        <div className="row my-3 ">
-                            <div className="col-md-4">
-                                <label htmlFor="">Consumption</label>
-                                <input type="text" className="form-control" placeholder="123456" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Unite Type</label>
+                            <label htmlFor="">Unite</label>
                                 <select className="form-select" aria-label="Default select example">
                                     <option selected>Joule</option>
                                     <option value="1">kWh</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
                                 </select>
                             </div>
-
+                           
                             <div className="col-md-4">
-                                <label htmlFor="">Support Document (If Any)</label>
-                                <input type="file" className="form-control" placeholder="Upload document" />
+                            <label htmlFor="">calculation Method</label>
+                            <select className="form-select" aria-label="Default select example">
+                                    <option selected>Estimated</option>
+                                    <option value="1">kWh</option>
+                            </select>
                             </div>
+
                         </div>
+
+                       
                        </div>
 
 
                     <div className="my-4 d-flex justify-content-between">
                     <div>
-                    <button type="btn" className="btn btn-outline-secondary" onClick={handleClose}> Close </button>
+                    <button type="btn" className="btn btn-outline-secondary" onClick={handleClose}> Cancel </button>
                     </div>
                     <div>
-                    <button type="btn" className="btn btn-outline-success" onClick={handleClose}> Save Changes </button>
+                    <button type="btn" className="btn btn-outline-success" onClick={handleClose}> Add </button>
                     </div>
                     </div>
                        </>
@@ -255,86 +205,72 @@ const Utilityprovidertable =()=>{
                         <>
                        <div>
                         <div className="d-flex align-items-center">
-                            <img src="../Images/EnergyComsuption_Modal.png" alt="" className="img-fluid"/>
+                            <img src="../Images/soldEnergy_Modal.png" alt="" className="img-fluid"/>
                             <div className="mx-2">
                                 <h4>Energy Consumption</h4>
-                                <h5>Utility Provider Energy</h5>
+                                <h5>Reduction of Energy Consumption</h5>
                             </div>
-
                         </div>
 
                         <div className="row mt-3">
                             <div className="col-md-4">
-                                <label htmlFor="">Energy Type</label>
-                                <select className="form-select" aria-label="Default select example">
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                <label htmlFor="">Initiative Name</label>
+                                <input type="date" className="form-control" placeholder="LED Lighting" />
+                            </div>
+
+                            <div className="col-md-4">
+                            <label htmlFor="">Source</label>
+                            <select className="form-select" aria-label="Default select example">
+                                    <option selected>Energy Consumption - Utility Provider Energy</option>
+                                    <option value="1">Wind</option>
                                 </select>
                             </div>
 
                             <div className="col-md-4">
-                                <label htmlFor="">Meter No.</label>
-                                <input type="text" className="form-control" placeholder="12345" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Account No.</label>
-                                <input type="text" className="form-control" placeholder="1234567890" />
+                            <label htmlFor="">Energy Amount</label>
+                                <input type="text" className="form-control" placeholder="24271"/>
                             </div>
 
                         </div>
 
                         <div className="row mt-3">
                             <div className="col-md-4">
-                                <label htmlFor="">Service Provider</label>
-                                <input type="text" className="form-control" placeholder="DAWA" />
+                                <label htmlFor="">Sold Energy</label>
+                                <input type="text" className="form-control" placeholder="264271" />
                             </div>
 
                             <div className="col-md-4">
-                                <label htmlFor="">Reading Date</label>
-                                <input type="text" className="form-control" placeholder="10 March 2024" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Meter Reading</label>
-                                <input type="text" className="form-control" placeholder="1234" />
-                            </div>
-
-                        </div>
-
-                        <div className="row my-3 ">
-                            <div className="col-md-4">
-                                <label htmlFor="">Consumption</label>
-                                <input type="text" className="form-control" placeholder="123456" />
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="">Unite Type</label>
+                            <label htmlFor="">Unite</label>
                                 <select className="form-select" aria-label="Default select example">
                                     <option selected>Joule</option>
                                     <option value="1">kWh</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
                                 </select>
                             </div>
-
+                           
                             <div className="col-md-4">
-                                <label htmlFor="">Support Document (If Any)</label>
-                                <input type="file" className="form-control" placeholder="Upload document" />
+                            <label htmlFor="">calculation Method</label>
+                            <select className="form-select" aria-label="Default select example">
+                                    <option selected>Estimated</option>
+                                    <option value="1">kWh</option>
+                            </select>
                             </div>
+
                         </div>
+
+                       
                        </div>
+
+
                     <div className="my-4 d-flex justify-content-between">
                     <div>
-                    <button type="btn" className="btn btn-outline-secondary" onClick={handleCloseEdit}> Close </button>
+                    <button type="btn" className="btn btn-outline-secondary" onClick={handleCloseEdit}> Cancel </button>
                     </div>
                     <div>
-                    <button type="btn" className="btn btn-outline-success" onClick={handleCloseEdit}> Save Changes </button>
+                    <button type="btn" className="btn btn-outline-success" onClick={handleCloseEdit}> Add </button>
                     </div>
                     </div>
                        </>
+
                         </Modal.Body>
                 </Modal>
 
@@ -372,4 +308,4 @@ const Utilityprovidertable =()=>{
         </section>
     )
 }
-export default Utilityprovidertable;
+export default ReductionEnerguTable;
