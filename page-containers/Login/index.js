369031e74947e54630f659f3e3785b33fb15466a
Purchase Metaverk <satyendra.kumar@metaverk.io>
2024-06-17 17:47:05 +0530

    Login update

--- page-containers/Login/index.js
+++ page-containers/Login/index.js
@@ -81,9 +81,12 @@ function Login() {
           <div className="col-md-12 col-lg-9 col-xl-8">
             <div className={styles.adminLoginBox}>
               <div className="row" style={{ marginTop: "200px" }}>
-                <div className="col-md-6">
-                  <div><img src="./logo.png" alt="" /></div>
+                <div className="col-md-12">
+                  
                   <div className="py-5 ps-4 pe-3">
+                    <h1>Login</h1>
+                    <h6>Enter Your Email & Password</h6>
+                    
                     <form onSubmit={handleLogin}>
                       <div className="form-group mb-1">
                         <label>Email</label>
@@ -103,6 +106,7 @@ function Login() {
                       )}
                       <div className="form-group mt-lg-4 mt-3 position-relative mb-1">
                         <label>Password</label>
+                        <div className="position-relative">
                         <input
                           type={showPassword ? "text" : "password"}
                           className="form-control"
@@ -111,11 +115,13 @@ function Login() {
                           value={password}
                           onChange={(e) => setPassword(e.target.value)}
                         />
-                        <div className="showPasswordLogin">
+                        <div className="togglePasswordVisibility">
                           <span onClick={() => setShowPassword(!showPassword)} style={{ cursor: "pointer" }}>
-                          {showPassword ?  <img src='/images/eye.svg' alt='' /> :  <img src='/images/eyeClose.svg' alt='' />}
+                          {showPassword ?  <img src='/images/showPassword.png' alt='' style={{maxWidth:"30px"}}/> :  <img src='/images/hidePassword.png' alt='' style={{maxWidth:"30px"}}/>}
                           </span>
                         </div>
+                        </div>
+
                       </div>
                       {apiErrors.password && (
                         <span className="text-danger" style={{ fontSize: "14px" }}>
@@ -123,27 +129,57 @@ function Login() {
                         </span>
                       )}
                       <div className="form-group forget-password text-end mt-3 forgot">
+                        <div className="d-flex align-items-center justify-content-between">
+                        
+                        <div className="mb-3">
+                          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                          <label className="form-check-label mx-2 mt-1" for="exampleCheck1">Check me out</label>
+                        </div>
+                        
+                        <div>
                         <Link href="/admin/forget" className="inline-block">
                           Forgot Password?
                         </Link>
+                        </div>
+                        </div>
                       </div>
                       <div className="form-group mt-lg-4 mt-3">
-                        <button type="submit" className="btn btn-primary btn-lg btn-block">
-                          Login
+                        <button type="submit" className="btn btn-success btn-block">
+                          Login &#x2192;
                         </button>
                       </div>
-                      <p className="dontAccount mt-2">
-                        Don’t have an account?
+
+                      {/* Login With */}
+                      <div className="row my-5">
+                  <div className="col-md-12">
+                    <div className="text-center">
+                      <span className="loginWith_Section">or login with</span>
+                    </div>
+                    <div className="d-flex justify-content-evenly mt-5">
+                      <div className="loginWith_button"><img src="/Images/icons8-microsoft-365.svg" alt="" className="img-fluid"/></div>
+                      <div className="loginWith_button"><img src="/Images/icons8-google.svg" alt="" className="img-fluid"/></div>
+                    </div>
+                  </div>
+                </div>
+                      {/* Login With */}
+
+
+                      <div className="d-flex justify-content-between mt-4 align-items-center">
+                  <div style={{cursor:"pointer"}}>Don{"'"}t Have an Account?</div>
+                  
+                  <div>
+                  <Link href="/admin/signup"><button type="button" className="btn btn-outline-success">Create Account</button></Link>
+                  </div>
+                </div>
+
+                      {/* <p className="dontAccount mt-2">
+                        Don{"’"}t have an account?
+
                         <Link href="/admin/signup" className="mx-2">
                           SignUp
                         </Link>
-                      </p>
+                      </p> */}
                     </form>
-                  </div>
-                </div>
-                <div className="col-md-6 d-none d-md-block">
-                  <div className="footBallImg">
-                    <img className="img-fluid" src="../images/login.jpg" alt="./login.jpg" />
                   </div>
                 </div>
               </div>
