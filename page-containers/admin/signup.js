9e5b2a957ab7da0dff7d8738dbe29fbff62cd20e
pragyaUIdev <pragya@greensturn.com>
2024-06-19 18:12:16 +0530

    changes

--- page-containers/admin/signup.js
+++ page-containers/admin/signup.js
@@ -118,6 +118,7 @@ const SignupForm = () => {
     };
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // Validation checks
         if (formData.firstName.length < 1 || formData.firstName.length > 25) {
             setError("First Name must be between 1 and 25 characters");
             return;
@@ -126,7 +127,6 @@ const SignupForm = () => {
             setError("Last Name must be between 1 and 25 characters");
             return;
         }
-    
         if (!formData.email.includes('@') || formData.email.length > 50) {
             setError("Please enter a valid Email (up to 50 characters)");
             return;
@@ -136,14 +136,16 @@ const SignupForm = () => {
             setError("Password must be at least 8 characters long, include one capital letter, and one special symbol (!@#$%^&*)");
             return;
         }
+    
         const signupData = {
-            userName: formData.firstName+formData.lastName,
+            userName: formData.firstName + formData.lastName,
             email: formData.email,
-             packageID:1,
+            packageID: 1,
             password: formData.password,
             loginType: formData.role,
             country_id: formData.country
         };
+    
         try {
             const response = await fetch('http://3.108.58.161:3001/api/v1/users', {
                 method: 'POST',
@@ -157,7 +159,8 @@ const SignupForm = () => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status} - ${JSON.stringify(responseData)}`);
             }
-            console.log('Signup API Response:', responseData);
+    
+           
             setFormData({
                 firstName: '',
                 lastName: '',
@@ -169,11 +172,16 @@ const SignupForm = () => {
                 country: countries.length > 0 ? countries[0].info.longName : ''
             });
             setError(null);
+    
+           
+            window.location.href = "/login"; 
+    
         } catch (error) {
             console.error('Error signing up:', error.message);
             setError(`Failed to sign up: ${error.message}`);
         }
     };
+    
     return (
         <div className={styles.signupForm}>
             <h2>Sign Up</h2>
@@ -222,7 +230,7 @@ const SignupForm = () => {
             {error && <p className={styles.errorMsg}>{error}</p>}
 
             <div className={styles.loginContainer}>
-                <p>Already have an account? <a href="/login">Login</a></p>
+                <p>Already have an account? <a className='login' href="/login">Login</a></p>
             </div>
         </div>
     );
